c71aaf2883d309ca65c6388bc866511d80626ed6
Microsoft <121215879+Azansong@users.noreply.github.com>
2023-06-10 20:02:49 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib
 export PATH=$PATH:${ROOT_DIR}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 export PATH=$PATH:${ROOT_DIR}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 export PATH=$PATH:${ROOT_DIR}/prebuilts/misc/linux-x86/dtc
