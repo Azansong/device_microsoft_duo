28f825922ef5e65c91a42e05eefed30caa5205bf
Microsoft <121215879+Azansong@users.noreply.github.com>
2023-06-11 09:21:52 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib
-export LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN=prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
-export LINUX_GCC_CROSS_COMPILE_ARM32_PREBUILTS_BIN=prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
-export LZ4_PREBUILTS_BIN=prebuilts/misc/linux-x86/lz4
-export LIBUFDT_PREBUILTS_BIN=prebuilts/misc/linux-x86/libufdt
-export DTC_OVERLAY_TEST_EXT=${ROOT_DIR}/prebuilts/misc/linux-x86/libufdt/ufdt_apply_overlay
+export PATH=$PATH:${ROOT_DIR}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
+export PATH=$PATH:${ROOT_DIR}/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
+export PATH=$PATH:${ROOT_DIR}/prebuilts/misc/linux-x86/libufdt
+export PATH=$PATH:${ROOT_DIR}/prebuilts/misc/linux-x86/lz4
+export PATH=$PATH:${ROOT_DIR}/prebuilts/misc/linux-x86/libufdt/ufdt_apply_overlay
