82242103026daeac6991a6364bbfe539d18ceb37
Microsoft <121215879+Azansong@users.noreply.github.com>
2023-06-16 23:01:07 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,8 +1,1 @@
-export LINUX_GCC_CROSS_COMPILE_PREBUILTS_BIN=prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
-export LINUX_GCC_CROSS_COMPILE_ARM32_PREBUILTS_BIN=prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
 export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib
-export LZ4_PREBUILTS_BIN=prebuilts/misc/linux-x86/lz4
-export DTC_PREBUILTS_BIN=prebuilts/misc/linux-x86/dtc
-export LIBUFDT_PREBUILTS_BIN=prebuilts/misc/linux-x86/libufdt
-export DTC_EXT=${ROOT_DIR}/prebuilts/misc/linux-x86/dtc/dtc
-export DTC_OVERLAY_TEST_EXT=${ROOT_DIR}/prebuilts/misc/linux-x86/libufdt/ufdt_apply_overlay
