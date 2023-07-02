a6b47a0bd938b51156b14880ca7c90f1fbd5a87b
Microsoft <121215879+Azansong@users.noreply.github.com>
2023-07-02 18:16:30 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,7 @@ TW_H_OFFSET := -104
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 TARGET_USES_MKE2FS := true
-TW_NO_SCREEN_BLANK := true
+TW_NO_SCREEN_BLANK := false
 TW_EXCLUDE_APEX := true
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
